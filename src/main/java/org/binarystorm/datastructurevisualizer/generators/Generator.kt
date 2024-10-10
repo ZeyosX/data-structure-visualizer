f34668321393231b1c16b1cb9dab200107e79ba3
Omar Iseid <zeyos4gamer@gmail.com>
2024-10-10 10:15:24 +0300

    Added undo feature for generators

---
+++ src/main/java/org/binarystorm/datastructurevisualizer/generators/Generator.kt
@@ -0,0 +1,29 @@
+﻿package org.binarystorm.datastructurevisualizer.generators
+
+import net.minecraft.world.level.block.Block
+import java.util.Stack
+
+abstract class Generator(val size: Int) {
+    fun generate() {
+        val generatedBlock = doGenerate()
+        if (history.size > 10) {
+            history.removeAt(0)
+        }
+        history.add(generatedBlock)
+    }
+
+    abstract fun doGenerate(): ArrayList<ArrayList<Block>>
+
+    fun undo() {
+        if (history.isEmpty()){
+            return
+        }
+
+        val undoBlock = history.pop()
+
+    }
+
+    companion object {
+        private val history = Stack<ArrayList<ArrayList<Block>>>()
+    }
+}
